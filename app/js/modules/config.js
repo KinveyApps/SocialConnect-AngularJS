872b9d961c80b888c29e0db2956ac6af3b58c45b
Thomas Conner <thomas.conner@me.com>
2015-09-18 09:26:16 -0400

    Removing config.js
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- app/js/modules/config.js
+++
@@ -1,8 +0,0 @@
-var module = angular.module('config', []);
-module.constant('kinveyConfig', {
-  appKey: 'kid_byGoHmnX2',
-  appSecret: '9b8431f34279434bbedaceb2fe6b8fb5',
-  apiHostName: 'https://baas.kinvey.com'
-});
-
-window.facebookAppId = '540411052764485';
